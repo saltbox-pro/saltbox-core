ee85626d9688d29add3e7f4a3012e8194facf0a0
Anton Karmanov <a.karmanov@saltbox.pro>
2024-05-20 14:03:29 +0300

    ~ update SaltHttpClient
      - Fix relogin logic
      - Make code more compact

--- backend/app/salt_http_client.py
+++ backend/app/salt_http_client.py
@@ -1,9 +1,11 @@
+import asyncio
+import contextlib
 import logging
 
 from datetime import datetime, timedelta
 from functools import wraps
 from pprint import pformat
-from typing import Optional
+from typing import Awaitable, Optional
 
 import httpx
 import pydantic
@@ -35,22 +37,27 @@ class SaltHttpClientUnauthorized(SaltHttpClientBadResponse):
 
 def salt_http_client_login(fn):
     @wraps(fn)
-    async def wrapper(self: 'SaltHttpClient', *args, **kwargs):
-        if not self._token or self.token_expires:
-            LOGGER.info('Authenticate salt client due missing or expiring token')
+    async def wrapper(self: 'SaltHttpClient', *args, **kwargs) -> Awaitable:
+        if self.token_expires:
+            LOGGER.info(
+                'Authenticate salt client due to token expiration time %s',
+                self._token_expire.isoformat())
             await self._login()
-        for _ in range(1 + RETRIES_ON_AUTH_ERROR):
-            try:
-                return await fn(self, *args, **kwargs)
-            except SaltHttpClientUnauthorized:
-                LOGGER.warning('Try to reauthenticate salt client after authorization error')
+        for try_n in range(1, 1 + RETRIES_ON_AUTH_ERROR):
+            if try_n > 0:
+                LOGGER.warning(
+                    'Try %i to reauthenticate salt client after authorization error',
+                    try_n)
                 await self._login()
+            with contextlib.suppress(SaltHttpClientUnauthorized):
+                return await fn(self, *args, **kwargs)
+        raise SaltHttpClientUnauthorized('Failed to authenticate client')
+
     return wrapper
 
 
 class SaltHttpClient:
     """ SaltStack CherryPy Rest API client """
-    TOKEN_HEADER = 'X-Auth-Token'
     TOKEN_BEST_BEFORE_SEC = 3600
 
     def __init__(
@@ -61,62 +68,48 @@ class SaltHttpClient:
         strict_ssl=True,
         eauth='rest'
     ) -> None:
-        self._client_kwargs = {
-            'verify': strict_ssl,
-        }
-        self._instance = salt_instance
         self._login_data = {
             'username': username,
             'password': password,
             'eauth': eauth,
         }
-        self._default_headers = {
-            'Accept': 'application/json',
-        }
-        self._http: Optional[httpx.AsyncClient] = None  # TODO Use or delete
-        self._token = ''
+        self._http = httpx.AsyncClient(
+            base_url=salt_instance,
+            headers={'Accept': 'application/json', },
+            verify=strict_ssl,
+            proxies=None,
+        )
         self._token_expire = datetime.fromtimestamp(0.0)
+        self._loop = asyncio.get_event_loop()
 
-    def token_expires_in(self, seconds) -> bool:
-        return datetime.now() - self._token_expire < timedelta(seconds=seconds)
+    def __del__(self):
+        self._loop.run_until_complete(self._http.aclose())
+        self._loop.close()
+
+    def token_expires_in(self, seconds: int) -> bool:
+        return self._token_expire - datetime.now() < timedelta(seconds=seconds)
 
     @property
     def token_expires(self) -> bool:
         return self.token_expires_in(self.TOKEN_BEST_BEFORE_SEC)
 
-    def get_url(self, endpoint: str) -> str:
-        return f'{self._instance}/{endpoint}'
-
     async def _login(self) -> None:
-        if self._http is not None:
-            await self._http.aclose()
-        self._http = httpx.AsyncClient(**self._client_kwargs)
-        self._default_headers.pop(self.TOKEN_HEADER, None)
-
         try:
-            resp = await self._http.post(
-                url=self.get_url('login'),
-                headers=self._default_headers,
-                data=self._login_data)
+            resp = await self._http.post(url='login', data=self._login_data)
         except (httpx.HTTPError, ssl.SSLCertVerificationError) as error:
             msg = str(error)
             if not msg:
                 msg = type(error).__name__
             raise SaltHttpClientConnectionError(msg) from error
 
-        try:
-            resp.raise_for_status()
-        except httpx.HTTPStatusError as error:
-            raise SaltHttpClientBadResponse(error)
+        self._raise_for_status(resp)
 
         try:
             body = AuthResponse.model_validate_json(resp.text)
         except pydantic.ValidationError as err:
             raise SaltHttpClientBadResponse(err)
         ret = body.return_[0]
-        self._token = ret.token
         self._token_expire = datetime.fromtimestamp(ret.expire)
-        self._default_headers[self.TOKEN_HEADER] = self._token
 
     def _raise_for_status(self, response: httpx.Response) -> None:
         if response.status_code == 401:
@@ -149,11 +142,7 @@ class SaltHttpClient:
         }
 
         try:
-            async with httpx.AsyncClient(**self._client_kwargs) as http:
-                resp = await http.post(
-                    url=self.get_url('jobs'),
-                    headers=self._default_headers,
-                    data=data)
+            resp = await self._http.post(url='jobs', data=data)
         except (httpx.HTTPError, ssl.SSLCertVerificationError) as error:
             msg = str(error)
             if not msg:
