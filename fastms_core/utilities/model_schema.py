b5a2911dc7ef1b756d1e579d16da5ddda6430e6a
Alexey Baikov <a.baikov@saltbox.pro>
2024-10-14 12:53:34 +0300

    ~ fix types

--- fastms_core/utilities/model_schema.py
+++ fastms_core/utilities/model_schema.py
@@ -1,22 +1,26 @@
-from odmantic import Model
+from __future__ import annotations
+
+from odmantic import EmbeddedModel, Model
 from odmantic.field import ODMEmbedded
 
 
-def get_model_schema(model: Model, pre_path: str = None):
-    schema = list()
+def get_model_schema(model: type[Model] | type[EmbeddedModel], pre_path: str | None = None) -> list[dict[str, str]]:
+    schema = []
 
     odm_fields = model.__odm_fields__
 
     for field_name, field in model.model_fields.items():
-        full_field_name: str = f'{pre_path}.{field_name}' if pre_path else field_name
+        full_field_name = f'{pre_path}.{field_name}' if pre_path else field_name
         odm_field = odm_fields.get(field_name)
 
         if isinstance(odm_field, ODMEmbedded):
             schema.extend(get_model_schema(odm_field.model, full_field_name))
         else:
-            schema.append({
-                'name': full_field_name,
-                'label': field.title if field.title else full_field_name,
-            })
+            schema.append(
+                {
+                    'name': full_field_name,
+                    'label': field.title if field.title else full_field_name,
+                }
+            )
 
     return schema
