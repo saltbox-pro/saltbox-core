db5076ecc6ff25debb62463e399fed6573274e31
Ivan Moshkov <i.moshkov@saltbox.pro>
2025-01-20 21:18:15 +0300

    - Drop salt module

--- fastms_core/main.py
+++ fastms_core/main.py
@@ -1,5 +1,4 @@
 import logging
-
 from collections.abc import AsyncIterator
 from contextlib import asynccontextmanager
 
@@ -14,7 +13,6 @@ from fastms_core.dependencies import RolesRequiredDependency
 from fastms_core.jobs.router import router as jobs_router
 from fastms_core.jobs.router import ws_router as jobs_ws_router
 from fastms_core.minions.router import filters_router, minions_router
-from fastms_core.salt.router import router as salt_router
 from fastms_core.tasks.router import router as task_router
 
 LOGGER = logging.getLogger(__name__)
@@ -71,7 +69,6 @@ APP.include_router(
     dependencies=[Depends(RolesRequiredDependency(['default-roles-salt.box']))],
 )
 APP.include_router(jobs_ws_router)
-APP.include_router(salt_router)
 APP.include_router(
     task_router,
     dependencies=[Depends(RolesRequiredDependency(['default-roles-salt.box']))],
