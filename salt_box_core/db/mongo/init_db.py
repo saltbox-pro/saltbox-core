448d8a4d98b553501ecdbeeb72195e653a340dcb
Alexey Baikov <a.baikov@saltbox.pro>
2025-02-20 11:11:02 +0300

    + initialize SLS repos settings and create unique index

--- salt_box_core/db/mongo/init_db.py
+++ salt_box_core/db/mongo/init_db.py
@@ -5,6 +5,23 @@ from salt_box_core.db.mongo.config import get_mongo_db
 from salt_box_core.minion_collections.repositories.collection_repository import CollectionRepository
 from salt_box_core.minion_collections.schemas.collection_schemas import CollectionCreateSchema
 from salt_box_core.schema_sync.repository import JSONSchemaRepository
+from salt_box_core.sls_repos.repository import SettingsSlsRepoRepository
+
+
+async def init_sls_repos_settings() -> None:
+    db = get_mongo_db()
+    sls_settings_repo = SettingsSlsRepoRepository(db)
+
+    # Check existing indexes
+    indexes = sorted(await sls_settings_repo.collection.index_information())
+
+    # Create unique index for name if not exists
+    if 'name_unique_index_asc' not in indexes:
+        result = await sls_settings_repo.collection.create_index(
+            [('repo_url', pymongo.ASCENDING)], name='url_unique_index_asc', unique=True
+        )
+        logger.debug('Index created: %s', result)
+        logger.debug('Indexes: %s', indexes)
 
 
 async def init_json_schemas() -> None:
@@ -56,8 +73,12 @@ async def init_mongo_db() -> None:
 
     # Initialize minion_collections collection
     await init_collections()
-    logger.info('MongoDB collections initialized')
+    logger.debug('MongoDB collections initialized')
 
     # Initialize json_schemas collection
     await init_json_schemas()
-    logger.info('JSON schemas initialized')
+    logger.debug('JSON schemas initialized')
+
+    # Initialize sls_repos_settings collection
+    await init_sls_repos_settings()
+    logger.debug('SLS repos settings initialized')
