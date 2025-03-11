1fdb0b0adaa00f86f23c616ff5358e0cda9e5a59
Alexey Baikov <a.baikov@saltbox.pro>
2025-03-11 14:56:44 +0300

    + update router imports and include job schemas router

--- salt_box_core/main.py
+++ salt_box_core/main.py
@@ -7,13 +7,13 @@ from fastapi.middleware.cors import CORSMiddleware
 from salt_box_core.config import APP_NAME, SETTINGS, logger
 from salt_box_core.db.mongo.init_db import init_mongo_db
 from salt_box_core.db.redis import POOL
-from salt_box_core.jobs.router import router as jobs_router
-from salt_box_core.jobs.router import ws_router as jobs_ws_router
+from salt_box_core.jobs.routers.job_sc_router import router as job_schemas_router
+from salt_box_core.jobs.routers.jobs_router import router as jobs_router
+from salt_box_core.jobs.routers.jobs_router import ws_router as jobs_ws_router
 from salt_box_core.minion_collections.routers.collections_router import router as collections_router
 from salt_box_core.minion_collections.routers.filters_router import router as filters_router
 from salt_box_core.minion_collections.routers.minion_router import router as minions_router
-from salt_box_core.schema_sync.router import router as schema_sync_router
-from salt_box_core.sls_repos.routers.settings_router import router as settings_sls_router
+from salt_box_core.settings.routers.sls_repos_router import router as settings_sls_router
 from salt_box_core.tasks.routers.tasks_router import router as task_router
 from salt_box_core.tasks.routers.tasks_router import ws_router as task_ws_router
 from salt_box_core.tasks.routers.template_router import router as template_router
@@ -53,11 +53,11 @@ APP.add_middleware(
 
 APP.include_router(filters_router)
 APP.include_router(jobs_router)
+APP.include_router(job_schemas_router)
 APP.include_router(jobs_ws_router)
 APP.include_router(template_router)
 APP.include_router(task_router)
 APP.include_router(task_ws_router)
 APP.include_router(collections_router)
 APP.include_router(minions_router)
-APP.include_router(schema_sync_router)
 APP.include_router(router=settings_sls_router, prefix='/settings', tags=['Settings'])
