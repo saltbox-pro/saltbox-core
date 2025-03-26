09553ac0395426f6f76780b6684a76d03d0d2e77
Ivan Moshkov <i.moshkov@saltbox.pro>
2025-03-26 10:29:33 +0300

    * Fix MasterRepository.get_by_name_or_alias typing

--- salt_box_core/masters/repositories/master_repository.py
+++ salt_box_core/masters/repositories/master_repository.py
@@ -4,15 +4,13 @@ from fastapi import Depends
 from pymongo.asynchronous.database import AsyncDatabase
 
 from salt_box_core.db.mongo.config import get_mongo
-from salt_box_core.db.mongo.repository_base import BaseMongoRepository, ProjectionModel
+from salt_box_core.db.mongo.repository_base import BaseMongoRepository
 from salt_box_core.masters.schemas.master_schemas import MasterModel
 
 
 class MasterRepository(BaseMongoRepository[MasterModel]):
-    async def get_by_name_or_alias(
-        self, value: str, projection_model: type[ProjectionModel] | None = None
-    ) -> ProjectionModel:
-        return await self.get(query={'$or': [{'name': value}, {'alias': value}]}, projection_model=projection_model)
+    async def get_by_name_or_alias(self, value: str) -> MasterModel:
+        return await self.get(query={'$or': [{'name': value}, {'alias': value}]})
 
     class Meta:
         collection_name = 'master'
