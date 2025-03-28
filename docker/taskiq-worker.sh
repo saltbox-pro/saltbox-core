7095925841b029a6337ac88d31eef6f5b3097c68
Alexey Baikov <a.baikov@saltbox.pro>
2025-03-28 15:55:39 +0300

    ~ fix taskiq worker command

--- docker/taskiq-worker.sh
+++ docker/taskiq-worker.sh
@@ -7,7 +7,7 @@
 # -r - autoreload for dev
 # -w - number of workers
 # --max-fails - max number of failed tasks before stopping the worker
-cmd="taskiq worker -r salt_box_core.tkq:broker salt_box_core.jobs salt_box_core.settings -w 1 --max-fails 1"
+cmd="taskiq worker -r salt_box_core.tkq:broker salt_box_core.jobs.tasks salt_box_core.settings.tasks -w 1 --max-fails 1"
 
 echo "$ ${cmd}"
 eval "$cmd"
