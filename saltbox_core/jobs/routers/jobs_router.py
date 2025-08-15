8b68444b3b742e1b2a0fea2a78fb3218da89c9af
Alexey Baikov <a.baikov@saltbox.pro>
2025-08-15 13:58:46 +0300

    Refactor job router to enhance OpenAPI integration
    - Added OpenAPI extra configurations for job-related endpoints
    - Improved code organization and readability
    - Removed commented-out code for clarity

--- saltbox_core/jobs/routers/jobs_router.py
+++ saltbox_core/jobs/routers/jobs_router.py
@@ -13,6 +13,7 @@ from saltbox_core.jobs.schemas.job_schemas import (
     IntJid,
     JobCreateSchema,
     JobModel,
+    JobsActions,
     JobsListCursorRequest,
     JobsListRequest,
     JobsListResponse,
@@ -21,19 +22,26 @@ from saltbox_core.jobs.schemas.job_schemas import (
 from saltbox_core.jobs.services.job_services import JobServiceDependency
 from saltbox_core.utilities.jid import JID
 from saltbox_sdk.db.schemas_base import CursoredResponse, PaginatedResponse
+from saltbox_sdk.discovery_client.schemas import GatewayEndpointConfig
 
-router = APIRouter(
-    prefix='/jobs',
-    tags=['Jobs'],
-    responses={404: {'description': 'Not found'}},
+# from saltbox_sdk.fastapi_utils.dependencies import get_opa_query
+# from saltbox_sdk.utilities.helpers import match_query
+
+router = APIRouter(prefix='/jobs', tags=['Jobs'])
+
+
+@router.get(
+    '/cursored_list',
+    operation_id='jobs_list_cursor',
+    openapi_extra=GatewayEndpointConfig(
+        policy='core.jobs.list',
+        action=JobsActions.LIST,
+    ).model_dump(by_alias=True),
+    deprecated=True,
 )
-
-ws_router = APIRouter(prefix='/jobs')
-
-
-@router.get('/cursored_list', operation_id='jobs_list_cursor')
 async def jobs_list_cursor(
     request: Annotated[JobsListCursorRequest, Query()],
+    # opa_query: Annotated[dict, Depends(get_opa_query)],
     job_service: JobServiceDependency,
 ) -> CursoredResponse[JobsListResponse]:
     matches = []
@@ -43,8 +51,7 @@ async def jobs_list_cursor(
 
     if request.minion:
         matches.append(rf'"minions": \[*"{request.minion}"*\]')
-
-    return await job_service.get_list_cursored_by_dt(
+    result = await job_service.get_list_cursored_by_dt(
         start_datetime=request.start_datetime,
         end_datetime=request.end_datetime,
         cursor=request.cursor or 0,
@@ -52,11 +59,22 @@ async def jobs_list_cursor(
         match='*' + '*'.join(matches) + '*',
         projection_model=JobsListResponse,
     )
+    # filtred_data = [item for item in result.data if match_query(item.model_dump(), opa_query)]
+    # result.data = filtred_data
+    return result
 
 
-@router.get('', operation_id='jobs_list')
+@router.get(
+    '',
+    operation_id='jobs_list',
+    openapi_extra=GatewayEndpointConfig(
+        policy='core.jobs.list',
+        action=JobsActions.LIST,
+    ).model_dump(by_alias=True),
+)
 async def jobs_list(
     request: Annotated[JobsListRequest, Query()],
+    # opa_query: Annotated[dict, Depends(get_opa_query)],
     job_service: JobServiceDependency,
 ) -> PaginatedResponse[JobsListResponse]:
     start_datetime = request.start_datetime if not request.desc else request.end_datetime
@@ -69,10 +87,19 @@ async def jobs_list(
         desc=request.desc,
         projection_model=JobsListResponse,
     )
+    # filtred_data = [item for item in jobs.data if match_query(item.model_dump(), opa_query)]
+    # jobs.data = filtred_data
     return jobs
 
 
-@router.get('/{jid}', operation_id='job_retrieve')
+@router.get(
+    '/{jid}',
+    operation_id='job_retrieve',
+    openapi_extra=GatewayEndpointConfig(
+        policy='core.jobs.base',
+        action=JobsActions.READ,
+    ).model_dump(by_alias=True),
+)
 async def job_retrieve(
     jid: IntJid,
     job_service: JobServiceDependency,
@@ -81,7 +108,14 @@ async def job_retrieve(
     return await job_service.get_job(_jid)
 
 
-@router.post('', operation_id='job_create')
+@router.post(
+    '',
+    operation_id='job_create',
+    openapi_extra=GatewayEndpointConfig(
+        policy='core.jobs.base',
+        action=JobsActions.CREATE,
+    ).model_dump(by_alias=True),
+)
 async def job_create(
     item: CreateJobRequest,
     job_service: JobServiceDependency,
@@ -95,7 +129,14 @@ async def job_create(
     )
 
 
-@router.post('/sync_run', operation_id='job_create_sync')
+@router.post(
+    '/sync_run',
+    operation_id='job_create_sync',
+    openapi_extra=GatewayEndpointConfig(
+        policy='core.jobs.base',
+        action=JobsActions.RUN,
+    ).model_dump(by_alias=True),
+)
 async def job_create_sync(
     item: CreateJobRequest,
 ) -> JobSyncResponse:
@@ -118,7 +159,14 @@ async def job_create_sync(
     return JobSyncResponse(**job_res.model_dump())
 
 
-@router.get('/{jid}/returns-count', operation_id='job_returns_count')
+@router.get(
+    '/{jid}/returns-count',
+    operation_id='job_returns_count',
+    openapi_extra=GatewayEndpointConfig(
+        policy='core.jobs.base',
+        action=JobsActions.READ,
+    ).model_dump(by_alias=True),
+)
 async def job_returns_count(
     jid: IntJid,
     job_service: JobServiceDependency,
@@ -131,7 +179,14 @@ async def job_returns_count(
     return await job_service.get_job_returns_count(JID(jid))
 
 
-@router.get('/{jid}/return', operation_id='job_returns_list')
+@router.get(
+    '/{jid}/return',
+    operation_id='job_returns_list',
+    openapi_extra=GatewayEndpointConfig(
+        policy='core.jobs.base',
+        action=JobsActions.READ,
+    ).model_dump(by_alias=True),
+)
 async def job_returns_list(
     jid: IntJid,
     job_service: JobServiceDependency,
