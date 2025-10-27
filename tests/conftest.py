957c2f1170d8f10988f21a44e8ce2336f33af47c
Ivan Moshkov <i.moshkov@saltbox.pro>
2025-10-27 18:39:30 +0300

    Add `find_one_and_update` to `AsyncMockCollection`

--- tests/conftest.py
+++ tests/conftest.py
@@ -68,6 +68,9 @@ class AsyncMockCollection:
         result = self.collection.update_one(filter, update, upsert=upsert)
         return self.mocker.MagicMock(modified_count=result.modified_count)
 
+    async def find_one_and_update(self, filter, update, upsert=False, session=None):
+        return await self.update_one(filter=filter, update=update, upsert=upsert, session=session)
+
     async def delete_one(self, filter, session=None):
         """Эмуляция метода delete_one"""
         # Если в фильтре есть _id
