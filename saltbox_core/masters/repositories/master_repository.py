e23c1c1970b7872c68e62b8165391107d6e91272
Daniil Chistyakov <d.chistyakov@saltbox.pro>
2026-01-22 10:44:26 +0500

    SB-32. Add impl of create mongo collections

--- saltbox_core/masters/repositories/master_repository.py
+++ saltbox_core/masters/repositories/master_repository.py
@@ -1,7 +1,9 @@
 from typing import Annotated, ClassVar
 
+import pymongo
 from fastapi import Depends
 from pymongo.asynchronous.database import AsyncDatabase
+from pymongo.operations import _IndexKeyHint
 
 from saltbox_core.masters.schemas.master_schemas import MasterModel
 from saltbox_sdk.db.mongo.config import get_mongo
@@ -9,13 +11,16 @@ from saltbox_sdk.db.mongo.repository_base import BaseMongoRepository
 
 
 class MasterRepository(BaseMongoRepository[MasterModel]):
-    async def get_by_master_id(self, value: str) -> MasterModel:
-        return await self.get(query={'master_id': value})
-
     class Meta:
         collection_name = 'master'
         auto_now_add_fields: ClassVar[list[str]] = ['created']
         auto_now_fields: ClassVar[list[str]] = ['modified']
+        collection_index_to_keys: ClassVar[dict[str, _IndexKeyHint]] = {
+            'master_id_unique_index_asc': [('master_id', pymongo.ASCENDING)]
+        }
+
+    async def get_by_master_id(self, value: str) -> MasterModel:
+        return await self.get(query={'master_id': value})
 
 
 def get_master_repository(db: Annotated[AsyncDatabase, Depends(get_mongo)]) -> MasterRepository:
--- saltbox_core/settings/repository.py
+++ saltbox_core/settings/repository.py
@@ -1,7 +1,9 @@
 from typing import Annotated, ClassVar
 
+import pymongo
 from fastapi import Depends
 from pymongo.asynchronous.database import AsyncDatabase
+from pymongo.operations import _IndexKeyHint
 
 from saltbox_core.settings.schemas.sls_repos_schemas import SettingsSlsRepoModel
 from saltbox_sdk.db.mongo.config import get_mongo
@@ -13,6 +15,10 @@ class SettingsSlsRepoRepository(BaseMongoRepository[SettingsSlsRepoModel]):
         collection_name = 'settings_sls_repos'
         auto_now_add_fields: ClassVar[list[str]] = ['created']
         auto_now_fields: ClassVar[list[str]] = ['modified']
+        collection_index_to_keys: ClassVar[dict[str, _IndexKeyHint]] = {
+            'name_unique_index_asc': [('repo_url', pymongo.ASCENDING)],
+            'local_path_unique_index_asc': [('local_path', pymongo.ASCENDING)]
+        }
 
 
 def get_sls_repo_repository(db: Annotated[AsyncDatabase, Depends(get_mongo)]) -> SettingsSlsRepoRepository:
