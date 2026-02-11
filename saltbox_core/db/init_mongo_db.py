d841b640bc127208aacd41a253c9a457894fc536
Alexey Baikov <a.baikov@saltbox.pro>
2026-02-11 13:22:23 +0300

    SB-51. Add PillarRepository to MongoDB initialization

--- saltbox_core/db/init_mongo_db.py
+++ saltbox_core/db/init_mongo_db.py
@@ -4,10 +4,12 @@ from saltbox_core.jobs.repositories.job_sc_repository import JobSchemaRepository
 from saltbox_core.masters.repositories.master_repository import MasterRepository
 from saltbox_core.minion_collections.repositories.collection_repository import CollectionRepository
 from saltbox_core.minion_collections.repositories.minion_repository import MinionRepository
+from saltbox_core.pillars_v2.repository import PillarRepository
 from saltbox_core.settings.repository import SettingsSlsRepoRepository
 from saltbox_core.tasks.repositories.tasks_minion import TaskMinionRepository
 from saltbox_core.tasks.repositories.tasks_template import TaskTemplateRepository
 from saltbox_sdk.db.mongo.config import get_mongo_db
+from saltbox_sdk.db.mongo.repository_base import BaseMongoRepository
 from saltbox_sdk.db.redis.config import get_redis_now
 
 
@@ -17,7 +19,7 @@ async def init_mongo_db() -> None:
     database = get_mongo_db()
     rdb = get_redis_now()
 
-    reps = [
+    reps: list[BaseMongoRepository] = [
         JobRepository(database),
         JobReturnRepository(database, rdb),
         JobSchemaRepository(database),
@@ -27,6 +29,7 @@ async def init_mongo_db() -> None:
         SettingsSlsRepoRepository(database),
         TaskMinionRepository(database),
         TaskTemplateRepository(database),
+        PillarRepository(database),
     ]
     for repo in reps:
-        await repo.create_collection()  # type: ignore
+        await repo.create_collection()
