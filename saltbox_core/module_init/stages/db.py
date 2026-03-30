7072d0792e942e35589d3878f344e2c0a3c8f5a5
Ivan Moshkov <i.moshkov@saltbox.pro>
2026-03-30 03:13:37 +0300

    SB-65. Add init migrations

--- saltbox_core/module_init/stages/db.py
+++ saltbox_core/module_init/stages/db.py
@@ -1,9 +1,22 @@
 from saltbox_core.config import logger
 from saltbox_core.db.init_mongo_db import init_mongo_db
+from saltbox_sdk.migrations.migrator import Migrator
 
 
 async def run_migrations() -> None:
     logger.info('Running DB migrations...')
+
+    migrator = Migrator(
+        modules_paths=[
+            ['saltbox_core', 'jobs'],
+            ['saltbox_core', 'masters'],
+            ['saltbox_core', 'minion_collections'],
+            ['saltbox_core', 'pillars'],
+            ['saltbox_core', 'settings'],
+            ['saltbox_core', 'tasks'],
+        ]
+    )
+    await migrator.migrate()
 
 
 async def run_stage() -> None:
